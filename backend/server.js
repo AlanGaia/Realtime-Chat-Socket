67d6383008ab9504689778ab384f18ad396969e6
Alan <devaliant94@gmail.com>
2020-06-22 20:17:21 -0300

    add get / Route

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ const io = socketio(server);
 //set Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Route /
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+})
 const serverBot = 'Dev BOT'
 
 //Run when Client Connects
